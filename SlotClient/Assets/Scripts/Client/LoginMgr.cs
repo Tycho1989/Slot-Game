d094c8acf2704a6dfd4cd9b9b759176c29ecd9e0
Tycho1989 <hjzqhd@qq.com>
2016-12-23 11:49:14 +0800

    添加登录流程文档

---
+++ SlotClient/Assets/Scripts/Client/LoginMgr.cs
@@ -0,0 +1,29 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* 文 件 名：LoginMgr.cs
+* 版权所有：	
+* 文件编号：
+* 创 建 人：Tycho
+* 创建日期：2016-12-22
+* 修 改 人：
+* 修改日期：
+* 描	述：业务逻辑类
+* 版 本 号：1.0
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using UnityEngine;
+using System.Collections;
+
+public class LoginMgr : MonoBehaviour
+{
+    //账号
+    private string userID;
+    public string UserID;
+
+    //密码
+    private string pwd;
+    public string Pwd;
+
+
+
+
+}
